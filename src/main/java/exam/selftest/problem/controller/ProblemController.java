62b3df4063716cb57360d7e457d82777dd5fdb21
closeup1202 <closeup1202@gmail.com>
2024-11-18 00:42:15 +0900

    first commit

---
+++ src/main/java/exam/selftest/problem/controller/ProblemController.java
@@ -0,0 +1,7 @@
+package exam.selftest.problem.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProblemController {
+}
---
+++ src/test/java/exam/selftest/SelfTestApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.selftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
